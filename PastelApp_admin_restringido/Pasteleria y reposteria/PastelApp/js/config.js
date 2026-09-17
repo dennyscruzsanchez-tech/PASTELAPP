6a513b14ee8aaa02679f34d06cde4ad223816cee
synned <dennys.cruzsanchez@gmail.com>
2026-09-17 08:45:20 -0500

    Update config.js

--- PastelApp_admin_restringido/Pasteleria y reposteria/PastelApp/js/config.js
+++ PastelApp_admin_restringido/Pasteleria y reposteria/PastelApp/js/config.js
@@ -1,1 +1,20 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyB_4YQggaD2n2M3IEw9CKBat4cheuo4RU0",
+  authDomain: "pastelapp-33e3f.firebaseapp.com",
+  projectId: "pastelapp-33e3f",
+  storageBucket: "pastelapp-33e3f.firebasestorage.app",
+  messagingSenderId: "335591117502",
+  appId: "1:335591117502:web:824cdcbdb4003136f544ef",
+  measurementId: "G-Z169BP0N76"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { app, auth, db };
