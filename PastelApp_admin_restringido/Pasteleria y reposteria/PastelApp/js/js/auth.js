6cf62dddf92327f6801142c179f05e825990a00e
synned <dennys.cruzsanchez@gmail.com>
2026-09-17 08:46:15 -0500

    Import Firebase Auth and Firestore in auth.js

--- PastelApp_admin_restringido/Pasteleria y reposteria/PastelApp/js/js/auth.js
+++ PastelApp_admin_restringido/Pasteleria y reposteria/PastelApp/js/js/auth.js
@@ -1,1 +1,13 @@
+import { auth, db } from "./config.js";
+import {
+    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.19.0/firebase-auth.js";
 
+import {
+    doc,
+    setDoc,
+    getDoc
+} from "https://www.gstatic.com/firebasejs/12.19.0/firebase-firestore.js";
+
+console.log("Firebase Authentication conectado correctamente");
